774cd64faf75ad4dbe898ee4451a99a3b5c814f2
Bruno Dias <brdiasdossantos@gmail.com>
2023-11-13 17:12:34 -0500

    Vignettes and documentation edition.

--- R/GEO712PACKAGE-package.R
+++ R/GEO712PACKAGE-package.R
@@ -12,7 +12,7 @@ NULL
 #' A dataset with house sale prices for King County, which includes Seattle.
 #' It includes homes sold between May 2014 and May 2015.
 #'
-#' @format A data frame with 21,613 rows and 14 variables:
+#' @format A data frame with 21,575 rows and 20 variables:
 #' \describe{
 #'   \item{price}{Sales price}
 #'   \item{bedrooms}{Number of bedrooms}
@@ -27,15 +27,21 @@ NULL
 #'   \item{sqft_above}{Area in square meters of the upper part of the house}
 #'   \item{sqft_basement}{Area in square meters of the basement}
 #'   \item{yr_built}{Year of construction}
-#'   \item{predicted_values}{Year of last renovation}
+#'   \item{yr_renovated}{Year of the last renovation}
+#'   \item{zipcode}{Zip code of the house}
+#'   \item{lat}{House latitude}
+#'   \item{long}{House longitude}
+#'   \item{sqft_living15}{Not detailed by the authors}
+#'   \item{sqft_lot15}{Not detailed by the authors}
+#'   \item{predicted_values}{Predicted value by the regression model}
 #' }
 #'
 #' @docType data
 #' @keywords datasets
-#' @name df
-#' @usage data(df)
+#' @name kc_house_data_processed
+#' @usage data(kc_house_data_processed)
 #' @source https://www.kaggle.com/datasets/harlfoxem/housesalesprediction/data
 #' @examples
-#'  data(df)
-#'  houses_prices <- df$price
-"df"
+#'  data(kc_house_data_processed)
+#'  houses_prices <- kc_house_data_processed$price
+"kc_house_data_processed"
